d4fcf34348278a07f772a647e0dacea89c130851
razvande1395 <105662540+razvande1395@users.noreply.github.com>
2025-09-19 14:38:09 +0300

    Update cleaning.py

--- python-scripts/cleaning.py
+++ python-scripts/cleaning.py
@@ -1,1 +1,38 @@
+import pandas as pd
 
+
+df = pd.read_csv("retail_sales_dataset.csv")
+
+
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+
+
+df = df.dropna(subset=["Date"])
+
+
+df["Computed_Total"] = df["Quantity"] * df["Price per Unit"]
+df["Total Amount"] = df["Computed_Total"]
+
+
+df = df.drop_duplicates()
+
+
+df["Month"] = df["Date"].dt.to_period("M").dt.to_timestamp()
+
+
+#creating a monthly revenue data set to use later vs forecasting
+monthly_sales = (
+    df.groupby("Month")["Total Amount"]
+    .sum()
+    .reset_index()
+    .rename(columns={"Total Amount": "Revenue"})
+)
+
+
+df.to_csv("retail_sales_clean.csv", index = False)
+monthly_sales.to_csv("retail_sales_monthly.csv", index = False)
+
+
+print("Cleaning complete. Files saved:")
+print("retail_sales_clean.csv (row-level cleaned data)")
+print("retail_sales_monthly.csv (monthly revenue)")
